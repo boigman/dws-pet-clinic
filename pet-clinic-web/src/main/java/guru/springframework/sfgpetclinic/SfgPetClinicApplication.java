7efa5af33dd675c75d04958c7f6f18897959b0df
08925 <08925@D5KYPQ2.lac1.biz>
2020-05-26 14:42:57 -0500

    Created JPA Service for owners. Closes #27.

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicApplication.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicApplication.java
@@ -1,10 +1,14 @@
 package guru.springframework.sfgpetclinic;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ImportResource;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+//import org.springframework.data.mongodb.core.MongoTemplate;
 
 @SpringBootApplication
+//@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class})
 //@ImportResource("classpath:wongo.xml")
 public class SfgPetClinicApplication {
 

ad47cdf779769c672747224753ec62cd86e1ffa7
08925 <08925@D5KYPQ2.lac1.biz>
2020-05-26 08:56:29 -0500

    Modified owners to JPA entities. Closes #23.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -2,12 +2,29 @@ package guru.springframework.sfgpetclinic.model;
 
 import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="pets")
 public class Pet extends BaseEntity{
 	
-	private PetType petType;
-	private Owner owner;
-	private LocalDate birthdate;
+	@Column(name="name")
 	private String name;
+
+	@ManyToOne
+	@JoinColumn(name="type_id")
+	private PetType petType;
+
+	@ManyToOne
+	@JoinColumn(name = "owner_id")
+	private Owner owner;
+	
+	@Column(name="birth_date")
+	private LocalDate birthdate;
 
 	public PetType getPetType() {
 		return petType;
@@ -15,6 +32,7 @@ public class Pet extends BaseEntity{
 	public void setPetType(PetType petType) {
 		this.petType = petType;
 	}
+	
 	public Owner getOwner() {
 		return owner;
 	}

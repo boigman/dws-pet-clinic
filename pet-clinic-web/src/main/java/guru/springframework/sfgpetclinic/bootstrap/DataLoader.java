7397b88a76ddb4f6ce735aa85e4641e5b67d9725
08925 <08925@D5KYPQ2.lac1.biz>
2020-05-27 07:24:39 -0500

    Implemented JPA Visit Service. Added to Data Loader. Closes #34.

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -11,10 +11,12 @@ import guru.springframework.sfgpetclinic.model.Pet;
 import guru.springframework.sfgpetclinic.model.PetType;
 import guru.springframework.sfgpetclinic.model.Specialty;
 import guru.springframework.sfgpetclinic.model.Vet;
+import guru.springframework.sfgpetclinic.model.Visit;
 import guru.springframework.sfgpetclinic.services.OwnerService;
 import guru.springframework.sfgpetclinic.services.PetTypeService;
 import guru.springframework.sfgpetclinic.services.SpecialtyService;
 import guru.springframework.sfgpetclinic.services.VetService;
+import guru.springframework.sfgpetclinic.services.VisitService;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -23,13 +25,15 @@ public class DataLoader implements CommandLineRunner {
 	private final VetService vetService;
 	private final PetTypeService petTypeService;
 	private final SpecialtyService specialtyService;
+	private final VisitService visitService;
 
 	public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService,
-			SpecialtyService specialtyService) {
+			SpecialtyService specialtyService, VisitService visitService) {
 		this.ownerService = ownerService;
 		this.vetService = vetService;
 		this.petTypeService = petTypeService;
 		this.specialtyService = specialtyService;
+		this.visitService = visitService;
 	}
 
 	private void loadData() {
@@ -82,8 +86,15 @@ public class DataLoader implements CommandLineRunner {
 		fionasCat.setBirthdate(LocalDate.now().minus(Period.ofDays(366)));
 		System.out.println(fionasCat.getName() + "'s Birthday: " + fionasCat.getBirthdate());
 		owner2.getPets().add(fionasCat);
-
+		
 		ownerService.save(owner2);
+
+		Visit catVisit = new Visit();
+		catVisit.setPet(fionasCat);
+		catVisit.setDate(LocalDate.now().minus(Period.ofDays(186)));
+		catVisit.setDescription("Sneezy kitty");
+
+		visitService.save(catVisit);
 
 		System.out.println("Loaded Owners...");
 
